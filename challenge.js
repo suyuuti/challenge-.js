cc8ca9623a12389372d4bc824a9917a716548db0
suyuuti <65999133+suyuuti@users.noreply.github.com>
2022-05-16 11:34:30 +0100

    Add files via upload

---
+++ challenge.js
@@ -0,0 +1,154 @@
+// var salary = parseInt(prompt("Enter your salary amount"));
+// var serviceYear = parseInt(prompt("Enter your service year"));
+
+// function bonus_anual(salary, serviceYear) {
+
+//   if (serviceYear >= 5) {
+//     var calculateBonus = 0.05 * salary;
+//     console.log("This your total take home " , calculateBonus + salary);
+//   } else {
+//     console.log("Sorry, you do not qualify for a bonus yet!");
+//   }
+
+// }
+// bonus_anual(salary, serviceYear);
+
+// var mark = parseInt(prompt("Enter your score"));
+// if (mark >= 80) {
+//   console.log("Hey, you have done well, got an A");
+// } else if (mark >= 60 && mark <= 79) {
+//   console.log("Awesome, you got B");
+// } else if (mark >= 50 && mark <= 59) {
+//   console.log("Good, you got a C");
+// } else if (mark >= 45 && mark <= 49) {
+//   console.log("Good, you got a D");
+// } else if (mark >= 25 && mark <= 44) {
+//   console.log("well, you got a E");
+// } else if (mark >= 0 && mark <= 24) {
+//   console.log("ops, you got failed");
+// } else {
+//   alert("Invalid input");
+// }
+
+/*
+var num = 6;
+if (num == 5 && num > 1 || num < 15 && num < 8 ) {
+  console.log("Yayyyy");
+}
+else{
+    console.log("and defeated")
+}
+*/
+
+// or
+
+// euality operators
+
+// var num1 = 5;
+// var num2 = "5";
+
+// if (num1 === num2) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+// return statment
+
+// function sayMyname(name){
+//    var sentence =  "my name is " + name;
+
+// }
+
+// console.log(sayMyname("Ayo"))
+
+// var arr1 = ["Ayo", "Chima", "Hadi"]
+// var result = arr1.pop()
+// console.log(result)
+
+//for loop
+//variable declaration/initialization
+//set my condition
+// for (var foo = 0; foo <= 10; foo+=5) {
+//   console.log(foo);
+// }
+
+// var arr = ["a", "b", "c"]
+// for(var i = 0; i < 3; i++){
+//     console.log(arr[i])
+// }
+// var arr = ["a", "b", "c"]
+// for(var i = 0; i < arr.length; i++){
+//     console.log(arr[i])
+//}
+                        //   ASSIGNMENT ONE
+// var Akin = parseInt (prompt("Akin, Enter your age"))
+// var Bola = parseInt (prompt("Bola, Enter your age"))
+// var Chidi = parseInt (prompt("Chidi, Enter your age"))
+
+// function ageBracket (Akin, Bola, Chidi){
+    
+//     console.log("the eldest of you is ", Math.max(Akin, Bola, Chidi), "years old")
+//     console.log("the youngest of you is ", Math.min(Akin, Bola, Chidi), "years old")
+
+// }
+// ageBracket(Akin, Bola, Chidi)
+
+
+                         
+// var classAttended =parseInt(prompt("how many classes did you attend"))
+// var classHeld =parseInt(prompt("how many classes were held"))
+
+// function classDetails(classAttended, classHeld){
+//     var examEntrance = ((classAttended/classHeld *100))
+//     if (examEntrance >= 75){
+//         console.log("Congratulations! you may enter exam hall")
+//     }
+//     else if(examEntrance < 75){
+//         console.log("Sorry, you do not qualify to sit for this exam!")
+//     }
+//     else{
+//         alert("Inavlid input")
+//     }
+// }
+// classDetails(classAttended, classHeld)
+
+                    //ASSIGNMENT TWO
+
+// var oddNumbers
+// for (oddNumbers = 1; oddNumbers< 100; oddNumbers+=2){
+//     console.log(oddNumbers)
+// }
+
+ 
+// var num = parseInt(prompt("enter number:"))
+//     function multi(){
+//         for (var multi = 1; multi <= 12; multi+=1){
+//             var product = (num * multi)
+//         console.log(`${num} * ${multi} = ${product} `)
+//         // return "multi"
+//      }
+//     }
+// multi()
+
+//   var sum = 0;
+//      for (var a = 0; a <= 10; a++){
+
+//         var sum = sum + a;
+//          console.log(sum)
+         
+//      }
+// var arr = ["a", "b", "c"]
+// for(var i = 0; i < 3; i++){
+//     console.log(arr[i])
+// }
+var arr = ["a", "b", "c"]
+for(var i = 0; i < arr.length; i++){
+    console.log(arr[i])
+}
+
+    // var arr = [0]
+    // for (var b = 0; b < 5; b++){
+    //     var arr = arr + b;
+    //     console.log(arr)
+        
+    // }
